b844c5106886b3cea0f1ee724a0f0b665a9d8826
Hypnos <47752595+Terryhata6@users.noreply.github.com>
2021-04-21 15:30:23 +0300

    FB+GA Integrated. Settings updated

---
+++ Assets/Scripts/SDKController.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Facebook.Unity;
+using GameAnalyticsSDK;
+
+public class SDKController : MonoBehaviour
+{
+    public void Awake()
+    {
+        FacebookInitialize();
+        GameAnalyticsInitialize();
+    }
+
+    private void GameAnalyticsInitialize()
+    {
+        GameAnalytics.Initialize();
+    }
+
+    private void FacebookInitialize()
+    {
+        if (!FB.IsInitialized)
+        {
+            // Initialize the Facebook SDK
+            FB.Init(InitCallback, OnHideUnity);
+        }
+        else
+        {
+            // Already initialized, signal an app activation App Event
+            FB.ActivateApp();
+        }
+    }
+    private void InitCallback()
+    {
+        if (FB.IsInitialized)
+        {
+            // Signal an app activation App Event
+            FB.ActivateApp();
+            // Continue with Facebook SDK
+            // ...
+        }
+        else
+        {
+            Debug.LogWarning("Failed to Initialize the Facebook SDK");
+        }
+    }
+    private void OnHideUnity(bool isGameShown)
+    {
+        if (!isGameShown)
+        {
+            // Pause the game - we will need to hide
+            Time.timeScale = 0;
+        }
+        else
+        {
+            // Resume the game - we're getting focus again
+            Time.timeScale = 1;
+        }
+    }    
+}
