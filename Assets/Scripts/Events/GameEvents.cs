a08f36331e7ad85a5385a683fbea3af37e4b13f2
Hypnos <47752595+Terryhata6@users.noreply.github.com>
2021-04-29 17:07:13 +0300

    Commit

---
+++ Assets/Scripts/Events/GameEvents.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEvents : MonoBehaviour
+{
+    public static GameEvents Current;
+
+    public void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        Current = this;        
+    }
+
+    #region GeneralEvents
+
+
+
+    #endregion
+
+    #region LevelEvents
+    public Action OnLevelStart;
+    public void LevelStart()
+    {
+        OnLevelStart?.Invoke();
+    }
+
+    public Action OnLevelEnd;
+    public void LevelEnd()
+    {
+        OnLevelEnd?.Invoke();
+    }
+
+    public Action OnLevelComplete;
+    public void LevelComplete()
+    {
+        OnLevelComplete?.Invoke();
+    }
+
+    public Action OnLevelFailed;
+    public void LevelFailed()
+    {
+        OnLevelFailed?.Invoke();
+    }
+    
+    #endregion
+
+}
